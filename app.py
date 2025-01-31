5263f1d04f5cedd680d556e7e2aa0d394ecc2f71
Yasasvi Krishna <pandu.krishna04@gmail.com>
2025-01-31 16:42:45 +0530

    222

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ class User(db.Model, UserMixin):
     password=db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    user_type=db.Column(db.String(20), nullable=False)
+    user_type=db.Column(db.String(120), nullable=False)
 
 
 @login_manager.user_loader
